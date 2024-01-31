7e3a07aaaa6985cfc946cce835e48112956c5c16
Mamba1990 <h.daoudim@outlook.com>
2024-01-31 04:36:45 -0800

    write Comment and Article class

--- models/Comment.py
+++ models/Comment.py
@@ -1,2 +1,24 @@
-#!/usr/bin/python3
+#!/usr/bin/python
 """ holds class Comment"""
+import models
+from models.BaseModel import BaseModel, Base
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String, ForeignKey
+
+
+class Review(BaseModel, Base):
+    """Representation of Comment """
+    if models.storage_t == 'db':
+        __tablename__ = 'comments'
+        article_id = Column(String(60), ForeignKey('articles.id'), nullable=False)
+        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+        text = Column(String(1024), nullable=False)
+    else:
+        article_id = ""
+        user_id = ""
+        text = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes Comment"""
+        super().__init__(*args, **kwargs)
--- models/__init__.py
+++ models/__init__.py
@@ -2,3 +2,17 @@
 """
 initialize the models package
 """
+
+from os import getenv
+
+
+storage_t = getenv("BB_TYPE_STORAGE")
+
+if storage_t == "db":
+    from models.engine.dbStorage import DBStorage
+    storage = DBStorage()
+else:
+    from models.engine.fileStorage import FileStorage
+    storage = FileStorage()
+storage.reload()
+
