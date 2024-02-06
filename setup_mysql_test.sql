d710cccba4af045e29cfeeba3ab56c43579db593
Mamba1990 <h.daoudim@outlook.com>
2024-02-06 13:35:26 -0800

    test_mysql_setting

---
+++ setup_mysql_test.sql
@@ -0,0 +1,7 @@
+-- prepares a MySQL server for the project Binary Briefs
+
+CREATE DATABASE IF NOT EXISTS bibr_test_db;
+CREATE USER IF NOT EXISTS 'bibr_test'@'localhost' IDENTIFIED BY 'Warrior@22@72';
+GRANT ALL PRIVILEGES ON `bibr_test_db`.* TO 'bibr_test'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'bibr_test'@'localhost';
+FLUSH PRIVILEGES;
